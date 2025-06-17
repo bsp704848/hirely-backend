64cd0049102fe43e5c9df98e6ddef16dc741cd8b
bsp704848 <bsp704848@gmail.com>
2025-06-17 15:47:02 +0530

    modified passport.js , auth.js

--- config/passport.js
+++ config/passport.js
@@ -1,13 +1,20 @@
+import dotenv from 'dotenv';
+dotenv.config();
 import passport from 'passport';
 import { Strategy as GoogleStrategy } from 'passport-google-oauth20';
 import User from '../models/User.js'; 
 
+console.log("Client ID:", process.env.GOOGLE_CLIENT_ID);
+
 passport.use(new GoogleStrategy({
+    
     clientID: process.env.GOOGLE_CLIENT_ID,
     clientSecret: process.env.GOOGLE_CLIENT_SECRET,
     callbackURL: 'https://hirely-backend.onrender.com/api/auth/google/callback',
 }, async (accessToken, refreshToken, profile, done) => {
     try {
+        console.log("✅ Google AccessToken:", accessToken);
+        console.log("👤 Google Profile:", profile);
         const existingUser = await User.findOne({ googleId: profile.id });
 
         if (existingUser) return done(null, existingUser);
@@ -21,6 +28,7 @@ passport.use(new GoogleStrategy({
 
         done(null, newUser);
     } catch (err) {
+        console.error("❌ Error in GoogleStrategy callback:", error);
         done(err, null);
     }
 }));
