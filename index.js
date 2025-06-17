9c8b77abb0b9caf8557539be2d149fb516fd44de
bsp704848 <bsp704848@gmail.com>
2025-06-17 14:50:38 +0530

    modified index.js

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ const app = express();
 app.use(cors({
   origin: [
     'http://localhost:5173',
-    'https://hirely-ten.vercel.app'
+    'https://hirely-app-mocha.vercel.app'
   ],
   methods: ['GET', 'POST', 'PUT', 'DELETE'],
   credentials: true
@@ -71,7 +71,7 @@ const io = new Server(server, {
   cors: {
     origin: [
       'http://localhost:5173',
-      'https://hirely-ten.vercel.app'
+      'https://hirely-app-mocha.vercel.app'
     ],
     methods: ["GET", "POST"],
     credentials: true
