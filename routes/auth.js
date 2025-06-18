e9e4db0f5b0a10403f69a15f2f712bb3d56f2b97
bsp704848 <bsp704848@gmail.com>
2025-06-18 15:58:10 +0530

    modified auth.js

--- routes/auth.js
+++ routes/auth.js
@@ -1,48 +1,61 @@
 
 import express from 'express'
 import passport from 'passport';
-import { registerUser, loginUser } from '../controllers/authController.js'
-import { getCurrentUser } from '../controllers/authController.js'
+import { registerUser, loginUser, getCurrentUser  } from '../controllers/authController.js'
 import { protect } from '../middleware/authMiddleware.js'
 import jwt from 'jsonwebtoken';
 
 const router = express.Router()
 
-router.get('/google', passport.authenticate('google', { scope: ['profile', 'email'] }));
 router.post('/register', registerUser)
 router.post('/login', loginUser)
 router.get('/me', protect, getCurrentUser) 
 
 
+router.get('/google', passport.authenticate('google', { scope: ['profile', 'email'] }));
 router.get('/google/callback',
   passport.authenticate('google', {
     failureRedirect: `${process.env.FRONTEND_URL}/login`,
     session: false
   }),
   async (req, res) => {
-    console.log('Setting token for user:', req.user?._id);
- 
-    const token = jwt.sign({ id: req.user._id }, process.env.JWT_SECRET, {
-      expiresIn: '7d',
-    });
-    
-    
-    res.cookie('token', token, {
-      httpOnly: true,
-      secure: true,
-      sameSite: 'None',
-      maxAge: 7 * 24 * 60 * 60 * 1000
-    });
-    
-    res.redirect(`${process.env.FRONTEND_URL}/`);
+    try {
+
+      const token = jwt.sign({ id: req.user._id }, process.env.JWT_SECRET, {
+        expiresIn: '7d',
+      });
+
+      res.cookie('token', token, {
+        httpOnly: true,
+        secure: true,
+        sameSite: 'None',
+        maxAge: 7 * 24 * 60 * 60 * 1000
+      });
+
+
+      console.log(`✅ Google Auth: Token created and cookie set for user ${req.user.email}`);
+
+     
+      res.redirect(`${process.env.FRONTEND_URL}/`);
+
+    } catch (error) {
+      console.error('Google Auth Callback Error:', error);
+      res.redirect(`${process.env.FRONTEND_URL}/login`);
+    }
   }
 );
+  
 
   
 router.get('/logout', (req, res) => {
-    req.logout(() => {
-        res.redirect('/');
-    });
+  res.clearCookie('token', {
+    httpOnly: true,
+    secure: true,
+    sameSite: 'None',
+  });
+  req.logout(() => {
+    res.redirect(`${process.env.FRONTEND_URL}/login`);
+  });
 });
 
 export default router
