2910ca3f6c4d6c19b23adae12c019e2749dbcb91
bsp704848 <bsp704848@gmail.com>
2025-06-18 17:47:11 +0530

    modified:   routes/auth.js

--- routes/auth.js
+++ routes/auth.js
@@ -26,7 +26,7 @@ router.get('/google/callback',
       });
 
       res.cookie('token', token, {
-        httpOnly: true,
+        httpOnly: false,
         secure: true,
         sameSite: 'None',
         maxAge: 7 * 24 * 60 * 60 * 1000
