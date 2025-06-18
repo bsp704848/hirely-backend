a03189817222f70b0d4f0dd29ceacf048d458c88
bsp704848 <bsp704848@gmail.com>
2025-06-18 16:52:23 +0530

    auth.js

--- routes/auth.js
+++ routes/auth.js
@@ -25,11 +25,17 @@ router.get('/google/callback',
         expiresIn: '7d',
       });
 
+      // res.cookie('token', token, {
+      //   httpOnly: true,
+      //   secure: true,
+      //   sameSite: 'None',
+      //   maxAge: 7 * 24 * 60 * 60 * 1000
+      // });
+
       res.cookie('token', token, {
-        httpOnly: true,
-        secure: true,
-        sameSite: 'None',
-        maxAge: 7 * 24 * 60 * 60 * 1000
+        httpOnly: false, // <-- important
+        sameSite: 'Lax',
+        path: '/'
       });
 
 
