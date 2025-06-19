d31cdff287e51b56d02ac6071aeeccc5043aa0f2
bsp704848 <bsp704848@gmail.com>
2025-06-19 16:43:28 +0530

    modified:index.js

--- index.js
+++ index.js
@@ -80,7 +80,11 @@ io.on('connection', (socket) => {
   })
 })
 
-app.set('io', io)
+app.set('io', io) 
+
+app.use((req, res, next) => {
+  res.status(404).json({ error: 'API route not found' });
+});
 
 server.listen(3000, () => {
   console.log('Server running on port 3000')
