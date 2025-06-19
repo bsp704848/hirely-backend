e198a0fe795b1ae6cd685f5de5da6ebc05603392
bsp704848 <bsp704848@gmail.com>
2025-06-19 09:53:59 +0530

    modified:   routes/auth.js

--- routes/auth.js
+++ routes/auth.js
@@ -27,7 +27,7 @@ router.get('/google/callback',
 
       res.cookie('token', token, {
         httpOnly: true,
-        secure: true,
+        secure: process.env.NODE_ENV === 'production',
         sameSite: 'None',
         maxAge: 7 * 24 * 60 * 60 * 1000
       });
