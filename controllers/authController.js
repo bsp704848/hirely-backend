75fee5aaecd12811106398f765c17d29d156d7c3
bsp704848 <bsp704848@gmail.com>
2025-06-20 15:34:41 +0530

    modified:   controllers/authController.js

--- controllers/authController.js
+++ controllers/authController.js
@@ -117,14 +117,22 @@ export const googleLogin = async (req, res) => {
 
     let user = await User.findOne({ email })
 
-    if (!user) {
+    if (user) {
+    
+      if (user.role !== role) {
+        return res.status(403).json({
+          message: `This email is already registered as ${user.role}. Please log in with that role.`,
+        });
+      }
+    } else {
+  
       user = await User.create({
         username: name,
-        email, 
+        email,
         role: role || 'employee',
         googleId,
         profilePic: picture,
-      })
+      });
     }
 
     const jwtToken = jwt.sign({ id: user._id }, process.env.JWT_SECRET, {
